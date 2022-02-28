5c70313309335979fe0c73516fdd5c1e53bca31b
josh-leebrowne <josh.lv.lee@gmail.com>
2022-02-27 19:46:01 -0600

    week1

---
+++ week1/to-do-list/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import ToDoList from './ToDo';
+
+function App() {
+  return (
+    <>
+      <ToDoList/>
+    </>
+  );
+}
+
+export default App;
---
+++ week1/to-do-list/src/ToDo.js
@@ -0,0 +1,16 @@
+function ToDoList () {
+        return(
+
+            <div>
+                <h1>To Do List</h1>
+                <ul>
+                    <input type="checkbox"/>Go to Gym<br></br>
+                    <input type="checkbox"/>Do Laundry<br></br>
+                    <input type="checkbox"/>Food Shopping<br></br>
+                    <input type="checkbox"/>Do Homework<br></br>
+                </ul>
+            </div>
+        );
+}
+    
+export default ToDoList;
